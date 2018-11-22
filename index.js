837791e34f9f0b84adbf52d979028a2eaa2d46b6
quan tran <tranmqt@gmail.com>
2018-11-21 22:59:04 -0800

    fix dependencies

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 /** @format */
 
+import React from 'react'
 import { AppRegistry } from 'react-native'
 import { Provider } from 'react-redux'
-import { createStore } from 'redux'
 import App from './components/App'
 import store from './redux/store'
 import { name as appName } from './app.json'
 
-const ReduxApp = () => {
+const ReduxApp = () => (
   <Provider store={store}>
     <App />
   </Provider>
-}
+)
 
 AppRegistry.registerComponent(appName, () => ReduxApp)
