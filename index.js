ea921d92b53d68c5a30845f6d42ad99b03f7eee8
oliverwang101 <oliverlw1336@gmail.com>
2019-01-15 22:52:51 -0800

    minor re-factor and name changed

--- index.js
+++ index.js
@@ -3,14 +3,29 @@
 import React from 'react'
 import { AppRegistry } from 'react-native'
 import { Provider } from 'react-redux'
-import App from './components/App'
+import Router from './components/Router'
 import store from './redux/store'
 import { name as appName } from './app.json'
+import firebase from 'firebase'
+class App extends React.Component {
+  componentWillMount(){
+    var config = {
+      apiKey: "AIzaSyDV6HAUZQbrfmdtCv7vPeiUpezqflrYozw",
+      authDomain: "polyrides-test2.firebaseapp.com",
+      databaseURL: "https://polyrides-test2.firebaseio.com",
+      projectId: "polyrides-test2",
+      storageBucket: "polyrides-test2.appspot.com",
+      messagingSenderId: "841128567403"
+    };
+    firebase.initializeApp(config);
+  }
+  render(){
+    return (
+      <Provider store={store}>
+        <Router />
+      </Provider>
+    );
+  }
+}
 
-const ReduxApp = () => (
-  <Provider store={store}>
-    <App />
-  </Provider>
-)
-
-AppRegistry.registerComponent(appName, () => ReduxApp)
+AppRegistry.registerComponent(appName, () => App)
