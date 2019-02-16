41a4084a98456897c7ed06a0efba0bf17eace638
Tyler Yun <tyler.yun1@gmail.com>
2019-02-15 19:43:35 -0800

    Added functionality for MapView. Also added SelectDate button for FindRide.

--- actionCreators/FindRide.js
+++ actionCreators/FindRide.js
@@ -5,7 +5,10 @@ import {
     START_LOCATION_UPDATED,
     DESTINATION_UPDATED,
     DEPARTURE_DATE_UPDATED,
-    RETURN_DATE_UPDATED
+    RETURN_DATE_UPDATED,
+    GET_RIDE_INPUT_DATA,
+    TOGGLE_SEARCH_RESULT_MODAL,
+    GET_CURRENT_LOCATION
     } from './types'
 import { Actions } from 'react-native-router-flux'
 import firebase from 'firebase'
@@ -19,3 +22,34 @@ export const roundtripUpdated = (roundTrip) => {
         })
     }
 }
+
+export const getRideInputData = ({ inputType, inputData }) => {
+    return {
+        type: GET_RIDE_INPUT_DATA,
+        payload: { inputType, inputData }
+    };
+};
+
+export const toggleSearchResultModal = (searchType) => {
+    return {
+        type: TOGGLE_SEARCH_RESULT_MODAL,
+        payload: searchType
+    };
+};
+
+//export const getAddressPredictions = ()
+
+export const getCurrentLocation = () => {
+    return (dispatch) => {
+		navigator.geolocation.getCurrentPosition(
+			position => {
+				dispatch({
+					type:GET_CURRENT_LOCATION,
+					payload:position
+				});
+			},
+			(error) => console.log(error.message),
+			{ enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 }
+		);
+	};
+};
--- components/rides/MapLocationPickerForm.js
+++ components/rides/MapLocationPickerForm.js
@@ -1,40 +1,110 @@
-import React from 'react'
-import {View} from 'react-native'
-import { MapView } from 'expo'
-import {SearchBox} from './SearchBox'
+import React, { Component } from 'react';
+import { View } from 'react-native';
+import { MapView } from 'expo';
+import { connect } from 'react-redux';
+import SearchBox from './SearchBox';
+import {
+   getRideInputData,
+   toggleSearchResultModal,
+   getCurrentLocation
+} from '../../actionCreators';
 
-class MapLocationPickerForm extends React.Component {
-    render(){
-        return(
-            <View style={styles.containerStyle}>
-                <MapView
-                    style={{ flex: 1 }}
-                    initialRegion={{
-                        latitude: 37.78825,
-                        longitude: -122.4324,
-                        latitudeDelta: 0.0922,
-                        longitudeDelta: 0.0421,
-                    }}
-                />
-                <SearchBox 
-                    // getInputData={getInputData}
-                    // toggleSearchResultModal={toggleSearchResultModal}
-                    // getAddressPredictions={getAddressPredictions}
-                    // selectedAddress={selectedAddress}
-                />
-            </View>
-           
-        )
-    }
-     
+class MapLocationPickerForm extends Component {
+   state = {
+      initialLat: 0,
+      initialLon: 0
+   };
+
+   componentDidMount() {
+      this.props.getCurrentLocation();
+   }
+
+   componentDidUpdate(prevProps, prevState) {
+      this.props.getCurrentLocation();
+   }
+
+   render() {
+      const { selectedPickUp, selectedDropOff } = this.props.selectedRouteAddresses || {};
+
+      const initialRegion = {
+         latitude: 37.548271,
+         longitude: -121.988571,
+         latitudeDelta: 0.0922,
+         longitudeDelta: 0.0421
+      };
+
+      return (
+         <View style={styles.containerStyle}>
+            { this.props.region.latitude &&
+            <MapView
+               style={{ flex: 1 }}
+               //initialRegion={this.props.region}
+               region={this.props.region}
+               showsUserLocation={true}
+            >
+
+               { selectedPickUp &&
+                  <MapView.Marker
+                     coordinate={{
+                        latitude: selectedPickUp.latitude,
+                        longitude: selectedPickUp.longitude
+                     }}
+                     pinColor="green"
+                  />	
+               }
+
+               { selectedDropOff &&
+                  <MapView.Marker
+                     coordinate={{
+                        latitude: selectedDropOff.latitude,
+                        longitude:selectedDropOff.longitude
+                     }}
+                     pinColor="blue"
+                  />	
+               }
+
+            </MapView>
+            }
+
+            <SearchBox
+               getRideInputData={this.props.getRideInputData}
+               toggleSearchResultModal={this.props.toggleSearchResultModal}
+               // getAddressPredictions={getAddressPredictions}
+               selectedAddress={this.props.selectedRouteAddresses}
+               selectedDate={this.props.selectedDate}
+            />
+
+            {/* Conditional Rendering of Search Results when toggleSearchResultModal is set */}
+            {  (this.props.searchResultTypes.pickUp || this.props.searchResultTypes.dropOff) &&
+               <SearchResults
+                  predictions={this.props.predictions}
+                  //getSelectedRouteAddresses={getSelectedRouteAddresses}
+               />
+			   }
+         </View> 
+      );
+   }
 }
-const styles = {
-    containerStyle: {
-        flex: 1,
-        flexDirection: 'column',
-        justifyContent:'center',      
-        backgroundColor: '#E4EDF2'
 
-    }
-  }
-export default MapLocationPickerForm
+const styles = {
+   containerStyle: {
+      flex: 1,
+      flexDirection: 'column',
+      justifyContent:'center',      
+      backgroundColor: '#E4EDF2'
+   }
+};
+
+const mapStateToProps = (state) => {
+   return {
+      selectedAddress: state.findRideForm.selectedAddress || {},
+      searchResultTypes: state.findRideForm.searchResultTypes || {},
+      predictions: state.findRideForm.predictions || [],
+      region: state.findRideForm.region,
+      selectedDate: state.findRideForm.selectedDate || {}
+   };
+};
+
+export default connect(mapStateToProps, {
+   getRideInputData, toggleSearchResultModal, getCurrentLocation
+})(MapLocationPickerForm);
